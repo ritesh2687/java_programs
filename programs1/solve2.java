379435fb2ae3223bca6b4eccc2c9bbdb83b5debf
ritesh2687 <riteshprajapati2687@gamil.com>
2024-08-02 22:53:30 +0530

    it is working

---
+++ programs1/solve2.java
@@ -0,0 +1,10 @@
+//Take name as input and print a greeting message for that particular name.
+import java.util.Scanner;
+public class solve2 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.print("please enter ur name=>....");
+        System.out.print("u are Welcomed here "+in.next());
+
+    }
+}
