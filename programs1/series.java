28e87fefaf08e01ac0ff280ada5a1a8e71b7421c
ritesh2687 <riteshprajapati2687@gamil.com>
2024-08-17 20:19:39 +0530

    Bubble sort class file

--- programs1/series.java
+++ programs1/series.java
@@ -2,7 +2,39 @@ package java_programs.programs1;
 
 import java.util.Scanner;
 
-public class series {
-    Scanner sc =new Scanner(System.in);
+public class Series {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter steps of series");
+        double num = sc.nextDouble();
+        System.out.println("enter the value of x");
+        double  x= sc.nextDouble();
+        double ans = seri(num,x);
+        System.out.println(ans);
     
+    sc.close();
+    }
+
+static double seri(double num,double x)
+{
+    double  sum=0;
+    for(double i=0;i<num;i++)
+    {double f=facto(num);
+    
+        sum+=((x*i)+(i+1))/f;
+    }
+     return sum;
+}
+
+static double facto(double num){
+    if (num==0 ) {
+    return 1;}
+    int  fact=1;
+    for (int  j=1;j<=num;j++)
+    {
+       fact=fact*j;
+    }
+    return fact;
+}
 }
